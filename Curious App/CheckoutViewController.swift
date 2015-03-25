5b9b077fb20a8f7b0d383c6545cbd293cd4113b1
anujverma <verma.anuj@gmail.com>
2015-03-25 12:05:08 -0700

    Preliminary storyboard set up for the final checkout view controller.

---
+++ Curious App/CheckoutViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CheckoutViewController.swift
+//  Curious App
+//
+//  Created by Anuj Verma on 3/25/15.
+//  Copyright (c) 2015 Anuj Verma. All rights reserved.
+//
+
+import UIKit
+
+class CheckoutViewController: UIViewController {
+
+    @IBOutlet weak var checkoutMainImage: UIImageView!
+    @IBOutlet weak var checkoutGiftBox: UIImageView!
+    @IBOutlet weak var checkoutGiftBoxTop: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
